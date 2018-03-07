0acd41f045332fca6f25400a90204a389fe9eb8f
Andrew Kairys <andrewkairys@gmail.com>
2018-03-07 20:21:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? do { |i| i.capitalize.concat(!)}
+  array.any? { |i| i.capitalize.concat(!)}
 end
 
 def find_the_cheese# code an argument here
