def5320f9a4b4d075e5cb3f580b7c661d0efc9de
Andrew Kairys <andrewkairys@gmail.com>
2018-03-07 20:43:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(array)
   array.any? { |i| i.length < 4 }
 end
 
-def find_the_cheese(array)
+def find_the_cheese?(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   array.find do |i|
